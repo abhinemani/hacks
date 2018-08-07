c5b3341bf3ff27a7b6f19b1b5a8b5e1a53b9fb52
Abhi Nemani <abhi.nemani@gmail.com>
2018-08-07 18:00:53 -0500

    edited inputs

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/12Qd_wiMLj8
 			for (var i=0; i<data.length; i++) {
 				str += "<section  class='future' data-background-image='img/ethosbg.png'>";
 				str += "<h1> <a target='_blank' href='" + data[i].Link + "'>" + data[i].Name + "</a></h1>";
-				str += "<h4>" + data[i].Category + "  // " + data[i].Applicant + "   <a target='_blank' href='mailto:" + data[i].Email + "'><i class='fa fa-envelope-o' aria-hidden='true'></i></a> // "+  data[i].Grant + "  // <a href='" + data[i].Link + "'><i class='fa fa-info-circle'></i></a></h4>";
+				str += "<h4>" + data[i].Tag + "  // " + data[i].Category + " // <a href='" + data[i].Link + "'><i class='fa fa-info-circle'></i></a></h4>";
 				str += "<p><strong>" + data[i].Description + "</strong></p>";
 				str += "<img src='img/"+data[i].Image+"'>";
 				str += "</section>";				
