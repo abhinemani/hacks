9b533d6cfd2efff4bd7abbe4d8edcfd45f278fa6
Abhi Nemani <abhi.nemani@gmail.com>
2018-08-07 22:04:58 -0500

    added set link for images

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/12Qd_wiMLj8
 				str += "<h1> <a target='_blank' href='" + data[i].Link + "'>" + data[i].Name + "</a></h1>";
 				str += "<h4>" + data[i].Category + "  // " + data[i].Tag + " // <a href='" + data[i].Link + "'><i class='fa fa-info-circle'></i></a></h4>";
 				str += "<p><strong>" + data[i].Description + "</strong></p>";
-				str += "<img src='img/"+data[i].Image+"'>";
+				str += "<img src='https://abhinemani.com/hacks/img/"+data[i].Image+"'>";
 				str += "</section>";				
 			}
 			gsheetClass.outerHTML=str; // it's simple replacement of whole element with contents of str var
