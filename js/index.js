535b65a31d56ddd8010329701abe4dc0c53e6c37
Abhi Nemani <abhi.nemani@gmail.com>
2018-08-07 21:05:35 -0500

    streamlined the copy for hacks specific deck

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/12Qd_wiMLj8
 			for (var i=0; i<data.length; i++) {
 				str += "<section  class='future' data-background-image='img/ethosbg.png'>";
 				str += "<h1> <a target='_blank' href='" + data[i].Link + "'>" + data[i].Name + "</a></h1>";
-				str += "<h4>" + data[i].Tag + "  // " + data[i].Category + " // <a href='" + data[i].Link + "'><i class='fa fa-info-circle'></i></a></h4>";
+				str += "<h4>" + data[i].Category + "  // " + data[i].Tag + " // <a href='" + data[i].Link + "'><i class='fa fa-info-circle'></i></a></h4>";
 				str += "<p><strong>" + data[i].Description + "</strong></p>";
 				str += "<img src='img/"+data[i].Image+"'>";
 				str += "</section>";				
