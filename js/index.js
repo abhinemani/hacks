974ff343ba3fb007268e4ff3d1ca5fd12e5bdd38
Abhi Nemani <abhi.nemani@gmail.com>
2018-08-07 22:04:50 -0500

    added set link for images

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@ var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/12Qd_wiMLj8
 		var gsheetClass = document.getElementsByClassName(target_gsheet_class_once)[0];
 		if(gsheetClass.outerHTML) { // if outerHTML is supported
 			for (var i=0; i<data.length; i++) {
-				str += "<section data-background-image='img/ethosbg.jpg'>";
+				str += "<section data-background-image='https://abhinemani.com/hacks/img/ethosbg.jpg'>";
 				str += "<h1> <a target='_blank' href='" + data[i].Link + "'>" + data[i].Name + "</a></h1>";
 				str += "<h4>" + data[i].Category + "  // " + data[i].Tag + " // <a href='" + data[i].Link + "'><i class='fa fa-info-circle'></i></a></h4>";
 				str += "<p><strong>" + data[i].Description + "</strong></p>";
